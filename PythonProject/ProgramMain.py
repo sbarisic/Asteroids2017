f8ef12fd1f98fc8f66734ebfb1a014c761213e3d
Saša Barišić <cartman300@net.hr>
2017-11-21 18:47:59 +0100

    Example obj models

--- PythonProject/ProgramMain.py
+++ PythonProject/ProgramMain.py
@@ -5,10 +5,15 @@ def main():
     Renderer.Initialize()
 
     while True:
-        Renderer.Clear(0, 0, 0)
+        Renderer.Clear((0, 0, 0))
 
         for i in range(100):
-            Renderer.Point(100 + i, 100, 255, 255, 255)
+            Renderer.Point((100 + i, 500), (255, 255, 255))
+
+        Renderer.Line((100, 100), (220, 240), (255, 255, 255))
+
+        #for i in range(100):
+        #    Renderer.Point((100 + i, 100), (255, 255, 255))
 
         Renderer.Update()
 
--- PythonProject/SoftwareRenderer.py
+++ PythonProject/SoftwareRenderer.py
@@ -24,13 +24,47 @@ class Renderer:
         return
 
     @staticmethod
-    def Clear(r, g, b):
+    def Clear(rgb):
         #sdl2.ext.fill(Renderer.WindowSurface, sdl2.ext.Color(r, g, b))
-        Renderer.Renderer.clear(sdl2.ext.Color(r, g, b))
+        Renderer.Renderer.clear(sdl2.ext.Color(*rgb))
         return
 
-    def Point(x, y, r, g, b):
-        Renderer.Renderer.draw_point((x, y), sdl2.ext.Color(r, g, b))
+    def Point(xy, rgb):
+        Renderer.Renderer.draw_point(xy, sdl2.ext.Color(*rgb))
+        return
+
+    def Line(start, end, rgb):
+        x0 = start[0]
+        y0 = start[1]
+        x1 = end[0]
+        y1 = end[1]
+        steep = False
+
+        if (abs(x0 - x1) < abs(y0 - y1)):
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+            steep = True
+        
+        if (x0 > x1):
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dx = x1 - x0
+        dy = y1 - y0
+        derror2 = abs(dy) * 2
+        error2 = 0
+        y = y0
+
+        for x in range(x0, x1):
+            if (steep):
+                Renderer.Point((y, x), rgb)
+            else:
+                Renderer.Point((x, y), rgb)
+
+            error2 += derror2
+            if (error2 > dx):
+                y += 1 if (y1 > y0) else -1
+                error2 -= dx * 2
         return
 
     @staticmethod
