df469a017cff2cdd959917b3bc61b74578485e9c
Saša Barišić <cartman300@net.hr>
2017-12-12 14:23:59 +0100

    Temporary

---
+++ PythonProject/ImePrezime.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+Name = "SAŠA BARIŠIĆ"
+
+W = 50
+H = 50
+
+X = -int(len(Name) / 2) * W
+
+def draw_char(c):
+	circle(50)
+	return
+
+
+for c in Name:
+	penup()
+	goto(X, 0)
+
+	draw_char(c)
+	pass
+
+while True:
+	pass
