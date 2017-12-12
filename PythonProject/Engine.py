757c0c99a0035639ff3d247ae4674693d2887772
Saša Barišić <cartman300@net.hr>
2017-12-12 14:24:05 +0100

    Physics

--- PythonProject/Engine.py
+++ PythonProject/Engine.py
@@ -5,27 +5,141 @@ import sfml.system
 import sfml.window
 
 from sfml import *
+from sfml.sf import Keyboard as Keys, Clock as Clock
 
-WIDTH = 800
-HEIGHT = 600
+TARGET_FPS = 60
 
-class Rocket():
+WIDTH = 1000
+HEIGHT = 800
+DEBUG = False
+
+LINEAR_DAMPENING = 0.8
+ANGULAR_DAMPENING = 0.5
+
+def vec_mul_scalar(s, vec):
+	return (vec[0] * s, vec[1] * s)
+
+def vec_sub_vec(a, b):
+	return (a[0] - b[0], a[1] - b[1])
+
+def vec_add_vec(a, b):
+	return (a[0] + b[0], a[1] + b[1])
+
+def setup_shape(S):
+	S.outline_color = sf.Color.WHITE
+	S.fill_color = sf.Color.TRANSPARENT
+	S.outline_thickness = 1
+
+def make_debug_shape(R):
+	D = sf.CircleShape()
+	setup_shape(D)
+	D.outline_color = sf.Color.RED
+
+	D.radius = R
+	D.origin = (R, R)
+	return D
+
+def calc_physics(self, dt, dampen):
+	if dampen:
+		self.linear_vel = vec_sub_vec(self.linear_vel, vec_mul_scalar(LINEAR_DAMPENING * dt, self.linear_vel))
+		self.angular_vel = self.angular_vel - (self.angular_vel * ANGULAR_DAMPENING * dt)
+
+	self.position = vec_add_vec(self.position, self.linear_vel)
+	self.angle = self.angle + self.angular_vel
+	return
+
+def draw_physics(self, wind):
+	self.Shape.position = self.position
+	self.Shape.rotation = 180 + self.angle
+	wind.draw(self.Shape)
+
+	if DEBUG:
+		self.Debug.position = self.Shape.position
+		wind.draw(self.Debug)
+
+	return
+
+
+class Bullet():
+	position = (0, 0)
+	angle = 0
+
+	linear_vel = (0, 0)
+	angular_vel = 0
+
 	def __init__(self):
-		self.Shape = sf.CircleShape()
+		S = sf.ConvexShape()
+		S.point_count = 4
+
+		Scale = 3
+		T = 0.25 / Scale
+
+		S.set_point(0, vec_mul_scalar(Scale, (-T, 2)))
+		S.set_point(1, vec_mul_scalar(Scale, (T, 2)))
+		S.set_point(2, vec_mul_scalar(Scale, (T, -2)))
+		S.set_point(3, vec_mul_scalar(Scale, (-T, -2)))
+
+		setup_shape(S)
+		self.Shape = S
+
+		if DEBUG:
+			self.Debug = make_debug_shape(Scale * 2)
 
 		return
 
 	def draw(self, wind):
-		wind.draw(self.Shape)
+		draw_physics(self, wind)
 		return
 
-def HandleEvents(W):
+	def update(self, dt):
+		calc_physics(self, dt, False)
+		return
+
+
+class Rocket():
+	position = (0, 0)
+	angle = 0
+
+	linear_vel = (0, 0)
+	angular_vel = 0
+
+	def __init__(self):
+		S = sf.ConvexShape()
+		S.point_count = 4
+
+		Scale = 5
+		S.set_point(0, vec_mul_scalar(Scale, (-2, -2)))
+		S.set_point(1, vec_mul_scalar(Scale, (0, 2)))
+		S.set_point(2, vec_mul_scalar(Scale, (2, -2)))
+		S.set_point(3, vec_mul_scalar(Scale, (0, -1)))
+
+		setup_shape(S)
+		self.Shape = S
+
+		if DEBUG:
+			self.Debug = make_debug_shape(Scale * 2)
+
+		return
+
+	def draw(self, wind):
+		draw_physics(self, wind)
+		return
+
+	def update(self, dt):
+		calc_physics(self, dt, True)
+		return
+
+def HandleEvents(W, OnKey):
 	for event in W.events:
 		if event.type == sfml.window.Event.CLOSED:
 			W.close()
+		elif event == sf.Event.KEY_PRESSED or event == sf.Event.KEY_RELEASED:
+			OnKey(event == sf.Event.KEY_PRESSED, event["code"])
 
 	return
 
 def CreateWindow():
-	return sf.RenderWindow(sf.VideoMode(WIDTH, HEIGHT), "PyProject")
-
+	Ctx = sf.ContextSettings()
+	Ctx.antialiasing_level = 8
+	W = sf.RenderWindow(sf.VideoMode(WIDTH, HEIGHT), "PyProject", sf.Style.DEFAULT, Ctx)
+	return W
--- PythonProject/PythonProject.py
+++ PythonProject/PythonProject.py
@@ -1,29 +1,83 @@
 import os
 import Engine
 
+UP_KEYS = [ Engine.Keys.UP, Engine.Keys.W ]
+DOWN_KEYS = [ Engine.Keys.DOWN, Engine.Keys.S ]
+LEFT_KEYS = [ Engine.Keys.LEFT, Engine.Keys.A ]
+RIGHT_KEYS = [ Engine.Keys.RIGHT, Engine.Keys.D ]
+SHOOT_KEYS = [ Engine.Keys.SPACE ]
+
+Clock = Engine.Clock()
+DeltaTime = 0
+TurnAmount = 0
+MoveAmount = 0
+
 Window = None
 
-Rocket = Engine.Rocket()
+Bullet = Engine.Bullet()
+Bullet.position = (Engine.WIDTH / 2, Engine.HEIGHT / 2)
 
-def update():
+Rocket = Engine.Rocket()
+Rocket.position = (Engine.WIDTH / 2, Engine.HEIGHT / 2)
+
+Rocket.linear_vel = (0, 10)
+Rocket.angular_vel = 40
+
+def onKey(down, code):
+	global TurnAmount
+	global MoveAmount
+
+	if code in LEFT_KEYS:
+		TurnAmount = -1 if down else 0
+	if code in RIGHT_KEYS:
+		TurnAmount = 1 if down else 0
+	if code in UP_KEYS:
+		MoveAmount = 1 if down else 0
+	if code in DOWN_KEYS:
+		MoveAmount = -1 if down else 0
+
+	if down and (code in SHOOT_KEYS):
+		print("Pew pew!")
+
+	return
+
+def update(dt):
+	MaxVel = 20
+	TurnVelocity = 25 * TurnAmount * dt
+	if TurnAmount != 0 and abs(Rocket.angular_vel) < MaxVel:
+		Rocket.angular_vel = Rocket.angular_vel + TurnVelocity
+
+	Rocket.update(dt)
+	Bullet.update(dt)
+
+	Bullet.angle = Bullet.angle + 1
+
 	return
 
 def render():
 	Window.clear()
 
-	Rocket.draw(Rocket)
+	Rocket.draw(Window)
+	Bullet.draw(Window)
 
 	Window.display()
 	return
 
 def main():
 	global Window
+	global DeltaTime
+
 	Window = Engine.CreateWindow()
 
 	while Window.is_open:
-		Engine.HandleEvents(Window)
-		update()
+		Engine.HandleEvents(Window, onKey)
+		update(DeltaTime)
 		render()
+
+		while (Clock.elapsed_time.seconds < (1.0 / Engine.TARGET_FPS)):
+			pass
+
+		DeltaTime = Clock.restart().seconds
 
 	return 0
 
