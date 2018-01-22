f1d3c6b1f48dec1a97d72a0c8723a71570fdd97f
Saša Barišić <cartman300@net.hr>
2018-01-22 16:20:07 +0100

    Pack the game into standalone
    * Packs game into folder with standalone python interpreter and batch
    file to run it

---
+++ PackToZip.bat
@@ -0,0 +1,13 @@
+@echo off
+rmdir /S /Q temp_folder
+
+mkdir temp_folder
+mkdir temp_folder\AsteroidsGame\
+xcopy /E /Y python_bin temp_folder\AsteroidsGame\
+
+mkdir temp_folder\AsteroidsGame\Asteroids
+xcopy /E /Y bin temp_folder\AsteroidsGame\Asteroids\
+
+del temp_folder\AsteroidsGame\Asteroids\data.cfg
+
+echo start AsteroidsGame\pythonw.exe AsteroidsGame\Asteroids\PythonProject.py > temp_folder\Start_Asteroids.bat
